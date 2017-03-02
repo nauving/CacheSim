6f1a04579b9a479eae87be54e4df9418a2bdac46
nauving <nauving@gmail.com>
2017-03-02 15:10:55 -0800

    Create parser.cpp

---
+++ parser.cpp
@@ -0,0 +1,20 @@
+//file parser.cpp
+#pragma once
+
+#include <string.h>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+class Parser {
+  public:
+    Parser(string fname);
+    string Parser::GetNext();
+  private:
+    
+};
+
+Parser::Parser(string FileName){
+}
+
+
