42a0aac350b19c4a9ae7ab9276cb56df1e38c382
nauving <nauving@gmail.com>
2017-03-03 15:03:57 -0800

    Update parser.cpp

--- parser.cpp
+++ parser.cpp
@@ -1,24 +1,51 @@
 // File: parser.cpp
-// Parser is a class which handles the opening and parsing of a file containing data to be used by the
-// Cache Simulator. The Parser object will perform error checking on the data before passing it along
-// so that the Simulator is ensured to be receiving valid data.
+// Parser is a class which handles the opening and parsing of a file containing memory instructions to be used by the
+// Cache Simulator. The Parser object will perform error checking on the instruction before packaging it as a Memory
+// Instruction object and passing it along so that the Simulator is ensured to receive valid data.
 #pragma once
 
 #include <string>
 #include <iostream>
 #include <fstream>
+#include "MemInstr.h"
+
 using namespace std;
 
 class Parser {
   public:
-    Parser(string fname);       // Constructor forward declaration
+    Parser(string fname);       // Default constructor forward declaration
     string Parser::GetNext();   // GetNext forward declaration
   private:
-    
-    
+    string fileName;
+    ifstream dataFile;
+    boolean isOpen = FALSE;
+    Parser::open();
 };
 
-Parser::Parser(string FileName){
+Parser::Parser(){    // Default constructor
+  fileName = "input.txt";
+  dataFile(fileName);
 }
 
+Parser::Parser(string name){
+  fileName = name;
+  dataFile(fileName);
+}
 
+MemoryCommand Parser::GetNext(){
+  if (!isOpen){
+    openFile();
+  }
+  // start reading in lines from file and tokenize
+    
+}
+
+int Parser::openFile(){
+  if (!dataFile){ // make sure the file is good
+    return 0;
+  } else {
+    dataFile.open(fileName);
+    isOpen = TRUE;
+    return 1;
+  }
+}
