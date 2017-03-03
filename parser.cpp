4bddde41448c8b9df3d7c96bab9418830d261ce7
nauving <nauving@gmail.com>
2017-03-03 13:14:53 -0800

    Update parser.cpp

--- parser.cpp
+++ parser.cpp
@@ -1,16 +1,20 @@
-//file parser.cpp
+// File: parser.cpp
+// Parser is a class which handles the opening and parsing of a file containing data to be used by the
+// Cache Simulator. The Parser object will perform error checking on the data before passing it along
+// so that the Simulator is ensured to be receiving valid data.
 #pragma once
 
-#include <string.h>
+#include <string>
 #include <iostream>
 #include <fstream>
 using namespace std;
 
 class Parser {
   public:
-    Parser(string fname);
-    string Parser::GetNext();
+    Parser(string fname);       // Constructor forward declaration
+    string Parser::GetNext();   // GetNext forward declaration
   private:
+    
     
 };
 
