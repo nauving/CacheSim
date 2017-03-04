0c0672b004d249991ba1f5e36872adf3ee5f22a2
ssalin <ssalin@pdx.edu>
2017-03-04 14:35:52 -0800

    added set ops

--- cache.h
+++ cache.h
@@ -2,29 +2,59 @@
 
 #define numsets = 1024;
 #define numlines = 4;
-#define lrusize = 2;
 
-class 
+
+struct
 line{
-  public:
-  //show line history
-  //add an item
-  private:
-  //address (without leading 0x)
-  //operation (r/w)
-  int lru; 
-  line * hist; //history of line
+  bool op; // 1 if read 0 if write;
+  line * next;	//history of line, 0 if no history
 };
 
 class 
 set{
   public:
-  add(); // add an item
+  add(char * toadd); // add an item
+  updatelru();
+  history(); //shpw t
   evict(); //remove an item 
   private:
-  line lines [numlines];//line data structure
-  int lru[lrusize]; //for updating LRU bits one just modified = 0, ++all others
+  struct line head [numlines];//linked list of lines, being head implies that that item is currently in the cache the rest are line history
+  int lru[numlines]; 	//// 0-3, 3 gets evicted on write
+  bool valid[numlines]; // 1 if data has been written
+  bool dirty[numlines]; // 1 if modified contents not yet written to memory
+  int hits;
+  int misses;
 };
+
+set::updatelru(){
+	for (int i = 0; i < numlines; ++i){
+		lru[i]+=1; //add one to all LRU bits
+	}
+}
+
+ set::add(void * toadd){
+	new line = toadd;
+	int leastrecent = -1; //se to negative bc 0 is a used index
+	//check valid
+	for(int i = 0; i < numlines; ++i){
+		if (valid[i]){ //line has good data
+			if (lru[i] == 3){
+				//is the item being replace dirty?
+				// if line op = read no need to update dirty
+				//if write need to update 
+				toadd->next = head[i]; //add head to list
+				head[i] = toadd; //make heat point at new data;
+				lru[i] = -1; //lets updatelru() work right
+				updatelru();
+				return 0;
+			}	
+		}
+		
+		else
+				//put in first invalid line
+	}
+	
+ }
 
 class 
 cache {
