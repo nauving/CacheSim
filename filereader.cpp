32b85559143bb94346ce05b9088d24f1a38a4b7e
nauving <nauving@gmail.com>
2017-03-07 12:22:31 -0800

    Update filereader.cpp

--- filereader.cpp
+++ filereader.cpp
@@ -2,9 +2,10 @@
 #include <iostream>
 #include <fstream>
 #include "parser.h"
+#include "cache.h"
 using namespace std;
 
-int main(argc, argv[]){
+/*int main(argc, argv[]){
 	string str;
 	cout << "Please enter the name of the file that contains the inputs: ";
 	cin >> str;
@@ -14,9 +15,9 @@ int main(argc, argv[]){
 	while(on){
 		on = c.unpack(parse);
 	}
-}
+}*/
 
-/*
+
 int main(int argc, char* argv[]) {
 	Parser ptest("file.txt");
 	MemInstr m;
@@ -24,12 +25,12 @@ int main(int argc, char* argv[]) {
 	do {
 		x++;
 		m = ptest.NxtPkg();
-		if (m.IsValid()) {
+		if (m.IsValid() && !p.IsEof()) {
 			cout << "*****************************\nAt dump " << x << ": " << m.Dump() << '\n';
 		}
 		cout << "\nLine is: " << ptest.PrintLine() << '\n' << "Token is: " << ptest.PrintToken() << "\n\n";
-	} while ((m.IsValid()) && (x <= 14));
+	} while ((m.IsValid()) && (!p.IsEof());
 	cin >> x;
 	return 0;
 }
-*/
+
